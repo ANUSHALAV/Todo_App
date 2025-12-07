15119f6b4ba4163c81d146a8f349b37ab536c16f
ANUSHALAV <bhattanushalav@gmail.com>
2025-12-07 20:42:20 +0530

    add header, add todo and show todo component and add bootstrap package

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './header/header';
+import { AddTodo } from './add-todo/add-todo';
+import { ShowTodo } from './show-todo/show-todo';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,Header,AddTodo,ShowTodo],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/show-todo/show-todo.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-show-todo',
+  imports: [],
+  templateUrl: './show-todo.html',
+  styleUrl: './show-todo.css',
+})
+export class ShowTodo {
+
+}
