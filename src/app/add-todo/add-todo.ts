37415aee5796b6c4f15e62415188251a8be72cba
ANUSHALAV <bhattanushalav@gmail.com>
2025-12-13 16:48:26 +0530

    completed UI of todo application

--- src/app/add-todo/add-todo.ts
+++ src/app/add-todo/add-todo.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-todo',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-todo.html',
   styleUrl: './add-todo.css',
 })
 export class AddTodo {
+
+  taskObj: any ={};
+  lstTasks: any = [];
+
+  addNewTask() {
+    this.taskObj.Task = this.taskObj.Task?.trim();
+
+    if (this.taskObj.Task == '' || this.taskObj.Task == undefined) {
+      alert("Please Enter a Task");
+      return;
+    }else{
+      alert("Task Added Successfully");
+    }
+  }
+
 
 }
